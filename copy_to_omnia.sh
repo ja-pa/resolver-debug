6dc4e80cacdf3eb0f17eba7c2eb76ec78d3d4c9d
Jan Pavlinec <jan.pavlinec@nic.cz>
2018-10-16 11:59:03 +0200

    resolver-debug: init LuCI app

---
+++ copy_to_omnia.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -f ".ssh_passwd" ]; then
+
+	sshpass -f .ssh_passwd	ssh root@192.168.1.1 'mkdir -p /usr/lib/lua/luci/controller/myapp'
+	sshpass -f .ssh_passwd	ssh root@192.168.1.1 'mkdir -p /usr/lib/lua/luci/view/myapp-mymodule'
+	sshpass -f .ssh_passwd	ssh root@192.168.1.1 'mkdir -p /usr/lib/lua/luci/model/cbi/myapp-mymodule'
+
+	sshpass -f .ssh_passwd scp src/new_tab.lua root@192.168.1.1:/usr/lib/lua/luci/controller/myapp/
+	sshpass -f .ssh_passwd scp src/view_tab.htm root@192.168.1.1:/usr/lib/lua/luci/view/myapp-mymodule/
+	sshpass -f .ssh_passwd scp src/cbi_tab.lua root@192.168.1.1:/usr/lib/lua/luci/model/cbi/myapp-mymodule/
+	sshpass -f .ssh_passwd scp src/cbi_file root@192.168.1.1:/etc/config/
+
+	sshpass -f .ssh_passwd ssh root@192.168.1.1 '/etc/init.d/lighttpd restart'
+else
+	echo "Error - password file not found!"
+	echo "Please make file .ssh_passwd with ssh password"
+fi
+
---
+++ src/cbi_tab.lua
@@ -0,0 +1,44 @@
+local util = require "luci.util"
+m = Map("cbi_file", translate("First Tab Form"), translate("Please fill out the form below")) -- cbi_file is the config file in /etc/config
+d = m:section(TypedSection, "info", "Part A of the form")  -- info is the section called info in cbi_file
+a = d:option(Value, "name", "Name"); a.optional=false; a.rmempty = false;  -- name is the option in the cbi_file
+
+
+o = d:option(TextValue, "_contact", "Kontakt")
+o.rmempty = false
+o.datatype = "string" 
+o.default="Ahoj lidi"
+o.description = "z.B. E-Mail, Telefon oder Chat-Name"  
+o.size = 35 
+
+f = d:option(Flag,"_flagaa","Test DNS")
+f.default=0
+
+
+
+
+btn = d:option(Button, "_btn", translate("Click this to run a script"))
+
+function btn.write()
+
+	luci.http.write("Haha, rebooting now...")
+	luci.template.render("myapp-mymodule/view_tab")
+    --o.default="Ahoj lidi jak se mate"
+    --luci.sys.call("/usr/bin/ls")
+    aa=luci.util.exec("ls /")
+    local field1 = luci.http.formvalue("cbid.cbi_file.A._contact")
+    local field2 = luci.http.formvalue("cbid.cbi_file.A._flagaa")
+
+    --ubus call resolver_rpcd.py list_dns '{}'
+    local dump = util.ubus("resolver_rpcd.py", "list_dns", { })
+    if field2 == nil then
+	luci.http.write(field1)
+	else
+	luci.http.write(field1..field2)
+	end
+	
+	luci.http.write_json(dump["list_dns"][1])
+
+end
+
+return m
