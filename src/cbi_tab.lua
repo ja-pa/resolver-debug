49348f22044bf2a872c8733cc95973fa98b7b4de
Jan Pavlinec <jan.pavlinec@nic.cz>
2018-10-26 16:15:03 +0200

    resolver-debug: fix luci rendering

--- src/cbi_tab.lua
+++ src/cbi_tab.lua
@@ -1,36 +1,45 @@
 local util = require "luci.util"
-m = Map("cbi_file", translate("First Tab Form"), translate("Please fill out the form below")) -- cbi_file is the config file in /etc/config
+m = Map("cbi_file", translate("DNS Resolver Debuger"), translate("This app should help you debug DNS resolver on your router.")) -- cbi_file is the config file in /etc/config
 d = m:section(TypedSection, "info", "Part A of the form")  -- info is the section called info in cbi_file
-a = d:option(Value, "name", "Name"); a.optional=false; a.rmempty = false;  -- name is the option in the cbi_file
 
 
-o = d:option(TextValue, "_contact", "Kontakt")
+
+o = d:option(TextValue, "domain", "Domain")
 o.rmempty = false
 o.datatype = "string" 
-o.default="Ahoj lidi"
-o.description = "z.B. E-Mail, Telefon oder Chat-Name"  
+o.default=""
+o.description = "Domain name which should be tested. You can put multiple domains separated by new line."  
 o.size = 35 
 
-f = d:option(Flag,"_flagaa","Test DNS")
+f = d:option(Flag,"_check_dnssec","Test DNSSEC")
 f.default=0
 
 
-function o.cfgvalue()
-	return "test necoho strasne dlouho"
-end
+---function o.cfgvalue()
+---	return "test necoho strasne dlouho"
+--end
 
 --aasdfasdf
-btn = d:option(Button, "_btn", translate("Click this to run a script"))
+btn_resolver = d:option(Button, "_btn_resolver", translate("Run resolver test"))
+btn_domain = d:option(Button, "_btn_domain", translate("Run domain test"))
 
-function btn.write()
 
-	luci.http.write("Haha, rebooting now...")
-	luci.template.render("myapp-mymodule/view_tab")
+function btn_resolver.write()
+
+    --luci.http.write("Haha, rebooting now...")
+    --    luci.template.render("admin_status/openvpnlog", {lua_log_variable=local_lua_log_variable, lua_title_variable="OpenVPN Log (tun)"})
+    --luci.http.redirect("tab_from_view",{domain="testik domain"})
+    
+    
+    --luci.template.render("myapp-mymodule/view_tab")
+    --http://192.168.2.1/cgi-bin/luci/admin/new_tab/tab_from_view
+    --No page is registered at '/admin/new_tab/myapp-mymodule/view_tab'.
     --o.default="Ahoj lidi jak se mate"
     --luci.sys.call("/usr/bin/ls")
     aa=luci.util.exec("ls /")
-    local field1 = luci.http.formvalue("cbid.cbi_file.A._contact")
-    local field2 = luci.http.formvalue("cbid.cbi_file.A._flagaa")
+    local field1 = luci.http.formvalue("cbid.cbi_file.A.domain")
+    local field2 = luci.http.formvalue("cbid.cbi_file.A._check_dnssec")
+    --o.write("Ahoj lidi")
 
     --ubus call resolver_rpcd.py list_dns '{}'
     --local dump = util.ubus("resolver-debugrpcd.py", "list_dns", { })
@@ -43,16 +52,16 @@ testik= [[<table>
 <tr><td>oskar.cz</td><td>OK</td><td>Failed</td></tr>
 <tr><td>wild.oskar.cz</td><td>OK</td><td>Failed</td></tr>
 </table>]]
+luci.template.render("myapp-mymodule/view_tab",{domain=testik})
  
-if field2 == nil then
-	luci.http.write(field1)
-else
-	luci.http.write(field1..field2)
-end
+--if field2 == nil then
+--   luci.http.write(field1)
+--else
+--    luci.http.write(field1..field2)
+--end
 
-	luci.http.write(testik)
-	
-	luci.http.write_json(dump)
+--    luci.http.write(testik)
+--    luci.http.write_json(dump)
 
 end
 
