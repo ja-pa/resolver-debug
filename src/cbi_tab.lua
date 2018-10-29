fde6436cfcd93a7b013768bb443725f9c1bcf2af
Jan Pavlinec <jan.pavlinec@nic.cz>
2018-10-29 15:54:28 +0100

    clean code

--- src/cbi_tab.lua
+++ src/cbi_tab.lua
@@ -24,11 +24,8 @@ btn_resolver = d:option(Button, "_btn_resolver", translate("Run resolver test"))
 btn_domain = d:option(Button, "_btn_domain", translate("Run domain test"))
 
 
-
-
-
 function test_dig_tbl()
-local util = require "luci.util"
+  local util = require "luci.util"
   ret_tbl="<div width=600px ><table>\n<tr><th>URL</th><th>Test result</th></tr>"
   t_dest={"www.nic.cz","www.seznam.cz"}
   -- URL, DNSSEC enabled , should pass
@@ -54,9 +51,9 @@ local util = require "luci.util"
     local state=""
     --local ret= util.ubus("resolver-debug.py", "test_dig",'{"domain":"' .. value[1] .. '","resolver":"8.8.8.8","dnssec":"true"}' )
     local ret= util.ubus("resolver-debug.py", "test_dig",
-    {domain=value[1],
-    resolver="8.8.8.8",
-    dnssec="true"} )
+                          {domain=value[1],
+                          resolver="8.8.8.8",
+                          dnssec="true"} )
     if ret["status"] == tostring(value[2]) then
       state="OK"
     else
@@ -71,45 +68,12 @@ local util = require "luci.util"
 end
 
 function btn_resolver.write()
-
-    --luci.http.write("Haha, rebooting now...")
-    --    luci.template.render("admin_status/openvpnlog", {lua_log_variable=local_lua_log_variable, lua_title_variable="OpenVPN Log (tun)"})
-    --luci.http.redirect("tab_from_view",{domain="testik domain"})
-    
-    
-    --luci.template.render("myapp-mymodule/view_tab")
-    --http://192.168.2.1/cgi-bin/luci/admin/new_tab/tab_from_view
-    --No page is registered at '/admin/new_tab/myapp-mymodule/view_tab'.
-    --o.default="Ahoj lidi jak se mate"
-    --luci.sys.call("/usr/bin/ls")
-    aa=luci.util.exec("ls /")
+    ---aa=luci.util.exec("ls /")
     local field1 = luci.http.formvalue("cbid.cbi_file.A.domain")
     local field2 = luci.http.formvalue("cbid.cbi_file.A._check_dnssec")
-    --o.write("Ahoj lidi")
 
-    --ubus call resolver_rpcd.py list_dns '{}'
-    --local dump = util.ubus("resolver-debugrpcd.py", "list_dns", { })
-    local dump = util.ubus("resolver-debug.py", "test_dig",'{"domain":"www.nic.cz","resolver":"8.8.8.8","dnssec":"true"}')
-    luci.http.write_json(dump["status"])
-    --'{"domain":"www.nic.cz","resolver":"8.8.8.8","dnssec":"true"}'
 
---# ubus -v call  resolver-debug.py "test_dig" '{"domain":"www.nic.cz","resolver":"8.8.8.8","dnssec":"true"}'
-
-testik= [[<table>
-<tr><th>Domain</th><th>DNS</th><th>DNSSEC</th></tr>
-<tr><td>oskar.cz</td><td>OK</td><td>Failed</td></tr>
-<tr><td>wild.oskar.cz</td><td>OK</td><td>Failed</td></tr>
-</table>]]
 luci.template.render("myapp-mymodule/view_tab",{domain=test_dig_tbl()})
- 
---if field2 == nil then
---   luci.http.write(field1)
---else
---    luci.http.write(field1..field2)
---end
-
---    luci.http.write(testik)
---    luci.http.write_json(dump)
 
 end
 
