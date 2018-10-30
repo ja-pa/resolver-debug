76d5f5355830a16a3a4d08a4b6181d26f45ff969
Jan Pavlinec <jan.pavlinec@nic.cz>
2018-10-30 13:24:49 +0100

    add custom domain testing

--- src/cbi_tab.lua
+++ src/cbi_tab.lua
@@ -4,6 +4,9 @@ d = m:section(TypedSection, "info", "Part A of the form")  -- info is the sectio
 
 
 
+btn_resolver = d:option(Button, "_btn_resolver", translate("Run resolver test"))
+
+
 o = d:option(TextValue, "domain", "Domain")
 o.rmempty = false
 o.datatype = "string" 
@@ -14,15 +17,20 @@ o.size = 35
 f = d:option(Flag,"_check_dnssec","Test DNSSEC")
 f.default=0
 
+btn_domain = d:option(Button, "_btn_domain", translate("Run domain test"))
 
 ---function o.cfgvalue()
 ---	return "test necoho strasne dlouho"
 --end
 
---aasdfasdf
-btn_resolver = d:option(Button, "_btn_resolver", translate("Run resolver test"))
-btn_domain = d:option(Button, "_btn_domain", translate("Run domain test"))
 
+function test_domain(domain,resolver,dnssec)
+  local ret= util.ubus("resolver-debug.py", "test_dig",
+                          {domain=domain,
+                          resolver=resolver,
+                          dnssec=dnssec})
+  return ret["status"]
+end
 
 function test_dig_tbl()
   local util = require "luci.util"
@@ -71,10 +79,35 @@ function btn_resolver.write()
     ---aa=luci.util.exec("ls /")
     local field1 = luci.http.formvalue("cbid.cbi_file.A.domain")
     local field2 = luci.http.formvalue("cbid.cbi_file.A._check_dnssec")
-
-
-luci.template.render("myapp-mymodule/view_tab",{domain=test_dig_tbl()})
-
+    --local aaa = aaa.
+    luci.template.render("myapp-mymodule/view_tab",{domain=test_dig_tbl()})
 end
+
+function btn_domain.write()
+    ---aa=luci.util.exec("ls /")
+    --local field1 = luci.http.formvalue("cbid.cbi_file.A.domain")
+    --local field2 = luci.http.formvalue("cbid.cbi_file.A._check_dnssec")
+    --local aaa = aaa.
+    local dnssec
+    local resolver="8.8.8.8"
+    local field1 = luci.http.formvalue("cbid.cbi_file.A.domain")
+     local field2 = luci.http.formvalue("cbid.cbi_file.A._check_dnssec")
+    local ret_tmp = "" -- ret_tmp .. field1 .. " <br> " .. "status=" .. bbb
+      if field2 == "1" then
+        dnssec="true"
+      else
+        dnssec="false"
+      end
+    
+    for line in string.gmatch(field1,'[^\r\n]+') do
+      --print( line=="" and "(blank)" or line)
+      local bbb = test_domain(line,"8.8.8.8",dnssec)
+      ret_tmp =ret_tmp .. line .. " " .. "status=" .. bbb .. "<br>" 
+    end
+    
+    luci.template.render("myapp-mymodule/view_tab",{domain=ret_tmp})
+end
+
+
 
 return m
